1afc58f0963ab2fce19b7a3d08329faf52636d88
babatunde tunde <tunde8983@gmail.com>
2021-11-24 11:05:12 +0100

    Added Shopping product list design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Announcement from '../components/Announcement';
 import Categories from '../components/Categories';
 import Navbar from '../components/Navbar';
+import Products from '../components/Products';
 import Slider from '../components/Slider';
 
 
@@ -11,6 +12,7 @@ const Home = () => {
            <Navbar />
            <Slider />
            <Categories />
+           <Products />
         </div>
     )
 }
