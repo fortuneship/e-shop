d1711585ab42ea2b081e36ec70fa21e3796ae9da
babatunde tunde <tunde8983@gmail.com>
2021-11-26 10:43:55 +0100

    Login and Register page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Home from './pages/Home';
-import ProductList from './pages/ProductList';
+import Login from './pages/Login';
 
 
 const App = () => {
   return (
-     <div><ProductList /></div>
+     <div><Login /></div>
     
   );
 }
