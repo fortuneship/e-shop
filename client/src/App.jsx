e7059deb22b7ce64a932ccf7b23357eaed9c73c2
babatunde tunde <tunde8983@gmail.com>
2021-12-29 01:22:32 +0100

    redux toolkit authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,12 @@ import Product from './pages/Product';
 import ProductList from "./pages/ProductList";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Success from './pages/Success';
+import { useSelector } from "react-redux";
 
 
 const App = () => {
 
-  const user = false;
+  const user = useSelector((state) => state.user.currentUser);
 
   return (
     <Router>
